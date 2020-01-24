d219fba78b2bb8d1b0897fb527d42d38ee76c24e
Mario Sánchez <mr.mario.sanchez@gmail.com>
2020-01-24 18:41:33 +0100

    feat: changes state batteries exports to be only one

--- packages/reffects-store/src/index.js
+++ packages/reffects-store/src/index.js
@@ -1,7 +1,3 @@
 export { default as store } from './store';
 export { default as subscribe } from './subscription';
-export {
-  default as batteries,
-  registerStateEffect,
-  registerStateCoeffect,
-} from './batteries';
+export { default as registerStateBatteries } from './batteries';
