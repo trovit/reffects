58d605a8f749731176ef3ef61707059a8c4b09b7
Mario Sánchez <mario.sanchez@lifullconnect.com>
2020-01-31 18:38:08 +0100

    chore: removes unused state effects and coeffects in example

--- examples/with-react-todos/src/effects/setState.js
+++
@@ -1,9 +0,0 @@
-import { registerEffectHandler } from 'reffects';
-
-export default function registerStateEffects(store) {
-  registerEffectHandler('setState', function setStateEffect(mutations) {
-    Object.entries(mutations).forEach(function([path, newValue]) {
-      store.setState({ path, newValue });
-    });
-  });
-}
