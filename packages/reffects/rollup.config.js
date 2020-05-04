0269e733c4eb476230e78e716f528f2aa2c92c35
trikitrok <trikitrok@gmail.com>
2020-05-04 18:56:49 +0200

    adding dependency on speco

--- packages/reffects/rollup.config.js
+++ packages/reffects/rollup.config.js
@@ -1,22 +1,28 @@
 import { terser } from 'rollup-plugin-terser'
+import pkg from './package.json';
+
+const external = Object.keys(pkg.dependencies);
 
 export default [
   // CommonJS
   {
     input: 'src/index.js',
     output: { file: 'dist/reffects.js', format: 'cjs', indent: false },
+    external,
   },
 
   // ES
   {
     input: 'src/index.js',
     output: { file: 'dist/reffects.es.js', format: 'es', indent: false },
+    external,
   },
 
   // ES for Browsers
   {
     input: 'src/index.js',
     output: { file: 'dist/reffects.mjs', format: 'es', indent: false },
+    external, 
     plugins: [
       terser({
         compress: {
@@ -36,8 +42,12 @@ export default [
       file: 'dist/reffects.umd.js',
       format: 'umd',
       name: 'reffects',
-      indent: false
+      indent: false,
+      globals: {
+        speco: 'speco',
+      },
     },
+    external, 
   },
 
   // UMD Production
@@ -47,8 +57,12 @@ export default [
       file: 'dist/reffects.umd.min.js',
       format: 'umd',
       name: 'reffects',
-      indent: false
+      indent: false,
+      globals: {
+        speco: 'speco',
+      },
     },
+    external, 
     plugins: [
       terser({
         compress: {
