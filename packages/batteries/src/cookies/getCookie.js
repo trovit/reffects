d1a3511ec0470d498adb654c19f98a52b1447a9e
Rubén Díaz Martínez <rubendm92@gmail.com>
2020-05-07 22:19:35 +0200

    feat: Rename cookie.get to cookie

--- packages/batteries/src/cookies/getCookie.js
+++ packages/batteries/src/cookies/getCookie.js
@@ -1,11 +1,11 @@
 import { coeffect, registerCoeffectHandler } from 'reffects';
 
 export function cookieGet(key) {
-  return coeffect('cookie.get', key);
+  return coeffect('cookie', key);
 }
 
 export default function registerGetCookieCoeffect(cookiesClient) {
-  registerCoeffectHandler('cookie.get', function cookie(key) {
+  registerCoeffectHandler('cookie', function cookie(key) {
     return {
       cookie: {
         [key]: cookiesClient.get(key),
