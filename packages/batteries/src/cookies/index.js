9d89fb5950e53a692c4d80d733de045d71c62b1b
Rubén Díaz Martínez <rubendm92@gmail.com>
2020-05-07 22:15:45 +0200

    feat: Implement builder for cookie.set

--- packages/batteries/src/cookies/index.js
+++ packages/batteries/src/cookies/index.js
@@ -1,8 +1,12 @@
-import setCookieEffect from './setCookie';
+import setCookieEffect, { cookieSet } from './setCookie';
 import getCookieCoeffect from './getCookie';
 
 export const registerSetCookieEffect = setCookieEffect;
 export const registerGetCookieCoeffect = getCookieCoeffect;
+
+export const cookies = {
+  set: cookieSet
+};
 
 export default function registerCookiesBatteries(cookieClient) {
   setCookieEffect(cookieClient);
