dcf247c1169675b97b38ea63f72376b24bcfa3e9
Rubén Díaz Martínez <rubendm92@gmail.com>
2020-05-08 17:41:42 +0200

    feat: expose http builders

--- packages/batteries/src/http/index.js
+++ packages/batteries/src/http/index.js
@@ -1,4 +1,11 @@
-import registerBatteries from './http';
+import registerBatteries, { httpGet, httpPost, httpPut, httpPatch } from './http';
+
+export const http = {
+  get: httpGet,
+  post: httpPost,
+  put: httpPut,
+  patch: httpPatch,
+};
 
 export default function registerHttpBatteries(httpClient) {
   registerBatteries(httpClient);
