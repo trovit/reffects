782a2b04957edbe4ab82c1abd257a0fe3f874169
Raúl Ferràs <raulferras@gmail.com>
2021-03-19 16:49:09 +0100

    exports http.delete builder

--- packages/batteries/src/http/index.js
+++ packages/batteries/src/http/index.js
@@ -1,10 +1,11 @@
-import registerBatteries, { httpGet, httpPost, httpPut, httpPatch } from './http';
+import registerBatteries, {httpGet, httpPost, httpPut, httpPatch, httpDelete} from './http';
 
 export const http = {
   get: httpGet,
   post: httpPost,
   put: httpPut,
   patch: httpPatch,
+  delete: httpDelete,
 };
 
 export default function registerHttpBatteries(httpClient) {
